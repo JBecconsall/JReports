2dfca435ca9c9189a3e1b11c7662746e94da449c
Joshua Becconsall <132403552+JBecconsall@users.noreply.github.com>
2023-12-05 22:27:29 +0000

    Initial commit

---
+++ src/main/java/me/josh/jreports/JReports.java
@@ -0,0 +1,17 @@
+package me.josh.jreports;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JReports extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("JReports online");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
